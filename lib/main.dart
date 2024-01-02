0216394556929b9c68baa684196e534e7b502039
dimassdoubles <codedimas@gmail.com>
2024-01-02 16:08:43 +0700

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:money_lover_clone/firebase_options.dart';
 import 'package:money_lover_clone/my_app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
