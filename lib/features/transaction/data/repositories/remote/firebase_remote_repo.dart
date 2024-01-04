33b764e32b06ee1bab61f06f0f8c461db32b8444
dimassdoubles <codedimas@gmail.com>
2024-01-05 02:53:43 +0700

    Feat: upload gambar

--- lib/features/transaction/data/repositories/remote/firebase_remote_repo.dart
+++ lib/features/transaction/data/repositories/remote/firebase_remote_repo.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:money_lover_clone/features/transaction/data/repositories/remote/transaction_remote_repo.dart';
@@ -10,7 +11,9 @@ import 'package:money_lover_clone/features/transaction/domain/models/transaction
 @LazySingleton(as: TransactionRemoteRepo)
 class FirebaseRemoteRepo implements TransactionRemoteRepo {
   static const collectionName = "transactions";
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   @override
   Future<void> addTransaction({
@@ -25,6 +28,20 @@ class FirebaseRemoteRepo implements TransactionRemoteRepo {
       String epochString = now.millisecondsSinceEpoch.toString();
       final String id =
           "TRX${now.year}${now.month.toString().padRight(2, '0')}${now.day.toString().padRight(2, '0')}-${epochString.substring(epochString.length - 4)}";
+      String downloadUrl = "";
+
+      if (image != null) {
+        String uniqueFilename =
+            DateTime.now().millisecondsSinceEpoch.toString();
+
+        Reference dirUpload =
+            _storage.ref().child('upload/${_auth.currentUser!.uid}');
+        Reference storedDir = dirUpload.child(uniqueFilename);
+
+        await storedDir.putFile(image);
+
+        downloadUrl = await storedDir.getDownloadURL();
+      }
 
       CollectionReference transactions = _firestore.collection(collectionName);
       transactions
@@ -36,7 +53,7 @@ class FirebaseRemoteRepo implements TransactionRemoteRepo {
               "categoryIconPath": category.iconPath,
               "categoryType": category.type.name,
               "description": description ?? "",
-              "image": "",
+              "image": downloadUrl,
             },
           )
           .then((value) => debugPrint("transaction added"))
